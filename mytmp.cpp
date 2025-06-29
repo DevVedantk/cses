ae8856c1b8aaa9af778166cbba5be9c6191e6de4
vedant-khasbage <vedantkhasbage2005@gmail.com>
2025-06-29 17:25:29 +0530

    template

---
+++ mytmp.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+
+
+using namespace std;
+const int INF=1e18;
+const int MOD=1e9+7;
+
+int findpow(int a,int b){
+    if(b==0) return 1;
+    int half=findpow(a,b/2);
+    if(b%2==0) return half*half;
+    else return a*half*half; 
+}
+
+int findceil(int a,int b){
+    return (a+b-1)/b;
+}
+
+
+signed main(){
+    int t;
+    cin >> t;
+    while(t-- >0){
+       
+
+
+    }  
+}
